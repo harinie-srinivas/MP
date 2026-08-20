21cba0f00e57ae3f69c4323fbd517c73dc3251c8
harinie-srinivas <chharinie291@gmail.com>
2026-08-20 22:46:55 +0530

    app1 commit

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!CMRCETuuuu" );
+        System.out.println( "Hello World!CMR" );
     }
 }
